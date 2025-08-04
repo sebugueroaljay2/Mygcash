99f054ae1efbe886001e73a1a47e7f25aab26c70
sebugueroaljay2 <sebugueroaljay2@example.com>
2025-08-04 13:31:10 +0800

    nine

--- resources/js/Pages/Gcash/Gcashv2.vue
+++ resources/js/Pages/Gcash/Gcashv2.vue
@@ -651,7 +651,7 @@ onMounted(() => {
                                         <select id="charge" v-model="form.charge_type_id"
                                             class="mx-auto py-2.5 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                             <option v-for="selection in props.charge_types" :key="selection.id"
-                                                :value="selection.id">{{ selection.charge }}</option>
+                                                :value="selection.id">{{ selection.charges }}</option>
                                         </select>
                                     </div>
                                     <div class="w-full">
