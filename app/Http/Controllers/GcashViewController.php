ffed47fff8a3a5d2bfe932d60b6aeefa46a89a08
sebugueroaljay2 <sebugueroaljay2@example.com>
2025-08-07 01:32:48 +0800

    adding

--- app/Http/Controllers/GcashViewController.php
+++ app/Http/Controllers/GcashViewController.php
@@ -10,14 +10,37 @@ use Inertia\Inertia;
 
 class GcashViewController extends Controller
 {
+    // public function index()
+    // {
+
+
+    //     return Inertia::render('Gcash/Gcashv2', [
+    //         'transaction_types' => TransactionType::all(),
+    //         'charge_types' => ChargeType::all(),
+    //         'allTransactions' => GcashTransaction::orderByDesc('id')->with('transaction_type', 'charge_type')->latest()->paginate(5),
+    //     ]);
+    // }
+
     public function index()
     {
+        $transactions = GcashTransaction::orderByDesc('id')
+            ->with('transaction_type', 'charge_type')
+            ->latest()
+            ->paginate(5);
 
-        
+        // Fix: replace null URLs with empty string
+        $cleanedLinks = collect($transactions->links())->map(function ($link) {
+            $link['url'] = $link['url'] ?? '';
+            return $link;
+        });
+
         return Inertia::render('Gcash/Gcashv2', [
             'transaction_types' => TransactionType::all(),
             'charge_types' => ChargeType::all(),
-            'allTransactions' => GcashTransaction::orderByDesc('id')->with('transaction_type', 'charge_type')->latest()->paginate(5),
+            'allTransactions' => [
+                ...$transactions->toArray(),
+                'links' => $cleanedLinks,
+            ]
         ]);
     }
 }
